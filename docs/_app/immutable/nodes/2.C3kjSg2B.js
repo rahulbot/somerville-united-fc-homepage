b202481a8db1a635b5391016d94c75c40cce1867
GitHub Action <action@github.com>
2026-01-06 14:56:45 +0000

    GH-Bot: Rebuild Svelte app for release

---
+++ docs/_app/immutable/nodes/2.C3kjSg2B.js
@@ -0,0 +1,131 @@
+import{d as g,a as c,f as u}from"../chunks/CwZspo-j.js";import"../chunks/Dt-q5NfI.js";import{Q as p,y as M,A as t,a3 as m,B as a,z as d,O as P,$ as A}from"../chunks/DHzX2w31.js";import{h as F}from"../chunks/CseRqWLw.js";import{s as S}from"../chunks/ChSk23Fx.js";import{D as I}from"../chunks/DzaAvLQn.js";import{I as y,s as f,l as U}from"../chunks/7G0YWGfT.js";import{l as b,s as _}from"../chunks/CdpCQCWD.js";function z(l,e){const s=b(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.555.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const i=[["path",{d:"M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"}]];y(l,_({name:"heart"},()=>s,{get iconNode(){return i},children:(o,n)=>{var r=g(),v=p(r);f(v,e,"default",{}),c(o,r)},$$slots:{default:!0}}))}function O(l,e){const s=b(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.555.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const i=[["path",{d:"M10 14.66v1.626a2 2 0 0 1-.976 1.696A5 5 0 0 0 7 21.978"}],["path",{d:"M14 14.66v1.626a2 2 0 0 0 .976 1.696A5 5 0 0 1 17 21.978"}],["path",{d:"M18 9h1.5a1 1 0 0 0 0-5H18"}],["path",{d:"M4 22h16"}],["path",{d:"M6 9a6 6 0 0 0 12 0V3a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1z"}],["path",{d:"M6 9H4.5a1 1 0 0 1 0-5H6"}]];y(l,_({name:"trophy"},()=>s,{get iconNode(){return i},children:(o,n)=>{var r=g(),v=p(r);f(v,e,"default",{}),c(o,r)},$$slots:{default:!0}}))}function N(l,e){const s=b(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.555.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const i=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"}],["path",{d:"M16 3.128a4 4 0 0 1 0 7.744"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87"}],["circle",{cx:"9",cy:"7",r:"4"}]];y(l,_({name:"users"},()=>s,{get iconNode(){return i},children:(o,n)=>{var r=g(),v=p(r);f(v,e,"default",{}),c(o,r)},$$slots:{default:!0}}))}const B="/_app/immutable/assets/soccer_team_of_young_men_huddling.KJgajEe-.png";var T=u(`<section class="hero svelte-juboms"><div class="hero-bg svelte-juboms"><img alt="Somerville United FC Team Huddle" class="hero-img svelte-juboms"/> <div class="overlay-gradient svelte-juboms"></div> <div class="overlay-dark svelte-juboms"></div></div> <div class="container hero-content-wrapper svelte-juboms"><div class="hero-content svelte-juboms"><div class="hero-badge animate-fade-in svelte-juboms" style="--delay: 0.2s">Est. 2025 • Massachusetts</div> <h1 class="hero-title animate-fade-in svelte-juboms" style="--delay: 0.4s">More Than <br/> <span class="highlight-text svelte-juboms">Just A Game</span></h1> <p class="hero-subtitle animate-fade-in svelte-juboms" style="--delay: 0.6s">Redefining soccer in Massachusetts by proving that passion, community, and opportunity can 
+        coexist sustainably. Joining the APSL in Spring of 2026.</p></div></div></section>`);function J(l,e){var s=T(),i=t(s),o=t(i);m(4),a(i),m(2),a(s),M(()=>S(o,"src",B)),c(l,s)}const L="/_app/immutable/assets/soccer_ball_on_grass_with_stadium_lights.DP2rz08o.png";var D=u('<section id="mission" class="section-py mission-section svelte-ouw4hy"><div class="container"><div class="mission-grid svelte-ouw4hy"><div class="mission-content"><h2 class="section-eyebrow">Our Mission</h2> <h3 class="section-heading">Uplifting Players. <br/> Mentoring Youth. <br/> Celebrating Somerville.</h3> <div class="mission-text svelte-ouw4hy"><p>Somerville deserves a club that belongs to everyone. A place where talent is nurtured, players are supported, and community comes first.</p> <p>Somerville United FC is here to:</p> <ul class="mission-list svelte-ouw4hy"><li class="svelte-ouw4hy"><span class="text-primary">✨</span> Provide pathways for local players</li> <li class="svelte-ouw4hy"><span class="text-primary">✨</span> Offer free/low-cost access to the game</li> <li class="svelte-ouw4hy"><span class="text-primary">✨</span> Create mentorship programs</li> <li class="svelte-ouw4hy"><span class="text-primary">✨</span> Reinvest profits back into Somerville</li> <li class="svelte-ouw4hy"><span class="text-primary">✨</span> Build a culture of unity, family, and ambition</li></ul></div> <div class="stats-grid svelte-ouw4hy"><div class="stat-card svelte-ouw4hy"><div class="stat-value svelte-ouw4hy">100%</div> <div class="stat-label svelte-ouw4hy">Community Owned</div></div> <div class="stat-card svelte-ouw4hy"><div class="stat-value svelte-ouw4hy">50+</div> <div class="stat-label svelte-ouw4hy">Local Players</div></div> <div class="stat-card svelte-ouw4hy"><div class="stat-value svelte-ouw4hy">1</div> <div class="stat-label svelte-ouw4hy">United Goal</div></div></div></div> <!></div></div></section>');function V(l){var e=D(),s=t(e),i=t(s),o=d(t(i),2);I(o,{get src(){return L},alt:"Soccer ball on field"}),a(i),a(s),a(e),c(l,e)}var E=u('<section class="section-py values-section svelte-k15d22"><div class="pattern-overlay svelte-k15d22"></div> <div class="container relative-z svelte-k15d22"><div class="values-header svelte-k15d22"><h2 class="values-eyebrow svelte-k15d22">Core Values</h2> <h3 class="values-title svelte-k15d22">Built on Strong Foundations</h3></div> <div class="values-grid svelte-k15d22"><div class="value-card group svelte-k15d22"><div class="card-content svelte-k15d22"><div class="icon-wrapper group-hover svelte-k15d22"><!></div> <h4 class="card-title svelte-k15d22">Passion</h4> <p class="card-text svelte-k15d22">Fueling every match, every practice, and every interaction with intense dedication and love for the game.</p></div></div> <div class="value-card group svelte-k15d22"><div class="card-content svelte-k15d22"><div class="icon-wrapper group-hover svelte-k15d22"><!></div> <h4 class="card-title svelte-k15d22">Community</h4> <p class="card-text svelte-k15d22">Building bridges, fostering inclusivity, and ensuring that the team belongs to the people of Somerville.</p></div></div> <div class="value-card group svelte-k15d22"><div class="card-content svelte-k15d22"><div class="icon-wrapper group-hover svelte-k15d22"><!></div> <h4 class="card-title svelte-k15d22">Opportunity</h4> <p class="card-text svelte-k15d22">Creating pathways for local talent to shine and providing mentorship for the next generation of players.</p></div></div></div></div></section>');function G(l){var e=E(),s=d(t(e),2),i=d(t(s),2),o=t(i),n=t(o),r=t(n),v=t(r);z(v,{class:"icon"}),a(r),m(4),a(n),a(o);var h=d(o,2),$=t(h),w=t($),C=t(w);N(C,{class:"icon"}),a(w),m(4),a($),a(h);var k=d(h,2),j=t(k),x=t(j),H=t(x);O(H,{class:"icon"}),a(x),m(4),a(j),a(k),a(i),a(s),a(e),c(l,e)}var R=u('<section id="community" class="section-py community-section svelte-1j74bpt"><div class="container"><div class="community-grid svelte-1j74bpt"><div class="community-content"><h2 class="section-eyebrow">Join The Family</h2> <h3 class="section-heading">Somerville Is Our Home. <br/> You Are Our Team.</h3> <div class="community-text svelte-1j74bpt"><p class="text-lg">We are building something special right here in Somerville. <br/><br/> If you’re a player, coach, parent, business, or supporter … this is your club too! <br/> <strong>Welcome to the beginning.</strong></p></div></div> <div class="community-image svelte-1j74bpt"><img alt="Somerville United FC Logo" class="logo-large svelte-1j74bpt"/></div></div></div></section>');function W(l){var e=R(),s=t(e),i=t(s),o=d(t(i),2),n=t(o);a(o),a(i),a(s),a(e),M(()=>S(n,"src",U)),c(l,e)}var Y=u('<meta name="description" content="Somerville United FC - Building community through soccer"/>'),K=u("<!> <!> <!> <!>",1);function ie(l){var e=K();F("1uha8ag",r=>{var v=Y();P(()=>{A.title="Somerville United FC - Home"}),c(r,v)});var s=p(e);J(s);var i=d(s,2);V(i);var o=d(i,2);G(o);var n=d(o,2);W(n),c(l,e)}export{ie as component};
