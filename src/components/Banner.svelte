481f266774bb0a331a69a66a00c6fc20c3503598
Rahul Bhargava <rahulbot@gmail.com>
2026-01-12 21:57:01 -0500

    update banner with news links

--- src/components/Banner.svelte
+++ src/components/Banner.svelte
@@ -1,11 +1,18 @@
 
 <div class="banner">
-    <div class="container">
-        Tryouts on Jan 18 
-        <a href="https://leguptraining.com/org_mjt1f396juzdk4/events">
-          <button class="btn btn-white register-btn">Register now!</button>
-        </a>
-    </div>
+  <div class="container">
+    📢 Catch us in the news! Featured on 
+    <a href="https://www.boston.com/community/sports-news/2026/01/12/somerville-couple-launches-new-affordable-soccer-club/">
+      Boston.com
+    </a>, 
+    <a href="https://wbznewsradio.iheart.com/content/somerville-couple-decides-to-found-their-own-non-profit-soccer-team/">
+      WBZ-1030
+    </a>
+    and 
+    <a href="https://patch.com/massachusetts/somerville/nonprofit-semi-pro-soccer-team-coming-soon-somerville">
+      Patch Somerville
+    </a>
+  </div>
 </div>
 
 
@@ -15,26 +22,14 @@
     padding: 20px;
     text-align: center;
     font-family: var(--font-heading);
-    font-weight: 800;
+    font-weight: 600;
     font-size: 2rem;
     color: white;
   }
 
-  .register-btn {
-    margin-left: 20px;
-    padding: 1rem;
-    font-size: 1.5rem;
-    transition: box-shadow 0.2s ease;
-    
-    a {
-      color: var(--primary);
-      font-weight: 600;
-      text-decoration: none;
-    }
-
-    &:hover {
-      box-shadow: 0 10px 25px rgba(0, 0, 0, 0.3);
-    }
-
+  a {
+    color: var(--background);
+    font-weight: 600;
+    text-decoration: underline;
   }
 </style>
