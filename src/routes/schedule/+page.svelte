43af886b9abb8469fdcc38992074a07f998cce0b
Rahul Bhargava <rahulbot@gmail.com>
2026-02-04 13:11:56 -0500

    countdown to home opener

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -2,6 +2,10 @@
   import { onMount } from 'svelte';
   import schedule from '@assets/data/schedule2026.json';
   import GameList from './GameList.svelte';
+
+  const today = new Date();
+  const firstGame = new Date(schedule[0].Date);
+  const countdownDays = Math.ceil((firstGame - today) / (1000 * 60 * 60 * 24));
 </script>
 
 <svelte:head>
@@ -12,7 +16,13 @@
 <div class="page-content">
   <div class="container">
     <section class="section">
-      <h1 class="page-title">Schedule</h1>
+      <h1 class="page-title">
+        {#if countdownDays > 0}
+          <em>{countdownDays}</em> Days Till Our Home Opener
+        {:else}
+          2026 Season Schedule
+        {/if}
+      </h1>
       <p class="page-subtitle">
         SUFC plays in the <a href="https://apslsoccer.com/APSL/Teams/">APSL Mayflower Conference</a>. Join one of our upcoming games.
       </p>
@@ -25,5 +35,9 @@
 <style>
   .section {
     padding: 2rem 0;
+  }
+  em {
+    color: var(--secondary);
+    font-style: normal;
   }
 </style>
