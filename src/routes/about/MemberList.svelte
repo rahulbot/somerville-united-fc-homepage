f95ab105b10d38d2ca9f2e3558907be52c4fcd93
Rahul Bhargava <rahulbot@gmail.com>
2026-03-09 21:34:25 -0400

    pull out managemnt team to its own page

---
+++ src/routes/about/MemberList.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Member from "./Member.svelte";
+  const {people} = $props();
+</script>
+
+<div class="mgmt-grid">
+  {#each people as person}
+    <Member 
+        name={person.name}
+        title={person.title}
+        description={person.description}
+        image={person.image}
+    />
+  {/each}
+</div>
+
+<style>
+  .mgmt-grid {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: 1fr;
+  }
+
+  @media (min-width: 640px) {
+    .mgmt-grid {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  @media (min-width: 1280px) {
+    .mgmt-grid {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
