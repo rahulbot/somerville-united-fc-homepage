abe23896e64ace7f99f9ba401a796ae6cab2a34e
GitHub Action <action@github.com>
2026-08-19 08:41:32 +0000

    GH-Bot: Rebuild Svelte app for release

---
+++ docs/_app/immutable/chunks/DHE-xcU8.js
@@ -0,0 +1,1 @@
+import{$ as a}from"./Blamb-uI.js";a();
---
+++ docs/_app/immutable/nodes/2.BaO0sXYn.js
@@ -0,0 +1,137 @@
+import{l as L,a as V,b as B,P as R,e as J}from"../chunks/BaBdxC8y.js";import{d as C,a as d,f as u}from"../chunks/BaHO-SBu.js";import{ax as z,a6 as s,aJ as b,a7 as a,a4 as n,aw as O,j as _,a2 as k,ay as E,aa as A,a3 as W,a5 as K}from"../chunks/Blamb-uI.js";import{i as $}from"../chunks/C3XpU74z.js";import{h as Q}from"../chunks/CTs_Fjy0.js";import"../chunks/DHE-xcU8.js";import{D as X}from"../chunks/CqP05iRC.js";import{I as H,s as Y}from"../chunks/CmZG7osN.js";import{l as T,s as P}from"../chunks/CCDQuTLm.js";import{s as G}from"../chunks/mCrG2AST.js";import{e as N,i as U}from"../chunks/CJ89sA4u.js";import{a as S}from"../chunks/Cq9HlXLZ.js";import{Y as Z}from"../chunks/DKYRhLgE.js";import{m as ee}from"../chunks/Cf6wP1QM.js";function ae(i,e){const t=T(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.555.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const r=[["path",{d:"M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"}]];H(i,P({name:"heart"},()=>t,{get iconNode(){return r},children:(o,m)=>{var l=C(),v=z(l);Y(v,e,"default",{}),d(o,l)},$$slots:{default:!0}}))}function se(i,e){const t=T(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.555.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const r=[["path",{d:"M10 14.66v1.626a2 2 0 0 1-.976 1.696A5 5 0 0 0 7 21.978"}],["path",{d:"M14 14.66v1.626a2 2 0 0 0 .976 1.696A5 5 0 0 1 17 21.978"}],["path",{d:"M18 9h1.5a1 1 0 0 0 0-5H18"}],["path",{d:"M4 22h16"}],["path",{d:"M6 9a6 6 0 0 0 12 0V3a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1z"}],["path",{d:"M6 9H4.5a1 1 0 0 1 0-5H6"}]];H(i,P({name:"trophy"},()=>t,{get iconNode(){return r},children:(o,m)=>{var l=C(),v=z(l);Y(v,e,"default",{}),d(o,l)},$$slots:{default:!0}}))}function te(i,e){const t=T(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.555.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const r=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"}],["path",{d:"M16 3.128a4 4 0 0 1 0 7.744"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87"}],["circle",{cx:"9",cy:"7",r:"4"}]];H(i,P({name:"users"},()=>t,{get iconNode(){return r},children:(o,m)=>{var l=C(),v=z(l);Y(v,e,"default",{}),d(o,l)},$$slots:{default:!0}}))}const re=!1,ie=async({fetch:i,params:e})=>({calendars:await L(i)}),Ge=Object.freeze(Object.defineProperty({__proto__:null,load:ie,ssr:re},Symbol.toStringTag,{value:"Module"}));var oe=u(`<section class="hero svelte-1sc0nj2"><div class="hero-bg svelte-1sc0nj2"><video autoplay="" loop="" playsinline="" class="hero-video svelte-1sc0nj2"><source src="/videos/stadium-loop.mov" type="video/mp4"/> Your browser does not support the video tag.</video> <div class="overlay-gradient svelte-1sc0nj2"></div> <div class="overlay-dark svelte-1sc0nj2"></div></div> <div class="container hero-content-wrapper svelte-1sc0nj2"><div class="hero-content svelte-1sc0nj2"><div class="hero-badge animate-fade-in svelte-1sc0nj2" style="--delay: 0.2s">Est. 2025 • Massachusetts</div> <h1 class="hero-title animate-fade-in svelte-1sc0nj2" style="--delay: 0.4s">Now Playing <span class="highlight-text svelte-1sc0nj2">more than just a game</span></h1> <p class="hero-subtitle animate-fade-in svelte-1sc0nj2" style="--delay: 0.6s">Redefining soccer in Massachusetts by proving that passion, community, and opportunity can 
+        coexist sustainably.</p></div></div></section>`,2);function le(i,e){var t=oe(),r=s(t),o=s(r);o.muted=!0,b(4),a(r),b(2),a(t),d(i,t)}var ne=u(`<section id="mission" class="mission-section"><div class="container"><h3>Uplifting Players. Mentoring Youth. Celebrating Somerville.</h3> <div class="mission-grid svelte-jfbymg"><div class="mission-content"><h2 class="section-eyebrow">Our Mission</h2> <div class="mission-text svelte-jfbymg"><p>Somerville deserves a club that belongs to everyone. A place where talent is nurtured, players are supported, and community comes first.</p> <p>Our <a href="team/mens">men's team</a> started playing in Spring of 2026, winning the APSL Mayflower Conference in their first season! 
+            Our <a href="team/womens">women's team</a> will follow in 2027. <a href="https://somervilleunitedfc.beehiiv.com">Subscribe to our newsletter</a> below to stay updated!</p></div></div> <div><p>Somerville United FC is here to:</p> <ul class="mission-list svelte-jfbymg"><li class="svelte-jfbymg"><span class="text-primary">✨</span> Provide pathways for local players</li> <li class="svelte-jfbymg"><span class="text-primary">✨</span> Offer free/low-cost access to the game</li> <li class="svelte-jfbymg"><span class="text-primary">✨</span> Create mentorship programs</li> <li class="svelte-jfbymg"><span class="text-primary">✨</span> Reinvest back into Somerville</li> <li class="svelte-jfbymg"><span class="text-primary">✨</span> Build a culture of unity, family, and ambition</li></ul></div></div> <!></div></section>`);function ve(i){var e=ne(),t=s(e),r=n(s(t),4);X(r,{aspectRatio:"25 / 9",src:"/images/action/apsl-mayflower-cup-raise.jpg",alt:"Raising the Mayflower Cup after clinching the conference title!"}),a(t),a(e),d(i,e)}var ce=u('<section class="values-section svelte-avlyqk"><div class="pattern-overlay svelte-avlyqk"></div> <div class="container relative-z svelte-avlyqk"><div class="values-header svelte-avlyqk"><h2 class="values-eyebrow svelte-avlyqk">Core Values</h2> <h3 class="values-title svelte-avlyqk">Built on Strong Foundations</h3></div> <div class="values-grid svelte-avlyqk"><div class="value-card group svelte-avlyqk"><div class="card-content svelte-avlyqk"><div class="icon-wrapper group-hover svelte-avlyqk"><!></div> <h4 class="card-title svelte-avlyqk">Passion</h4> <p class="card-text svelte-avlyqk">Fueling every match, every practice, and every interaction with intense dedication and love for the game.</p></div></div> <div class="value-card group svelte-avlyqk"><div class="card-content svelte-avlyqk"><div class="icon-wrapper group-hover svelte-avlyqk"><!></div> <h4 class="card-title svelte-avlyqk">Community</h4> <p class="card-text svelte-avlyqk">Building bridges, fostering inclusivity, and ensuring that the team belongs to the people of Somerville.</p></div></div> <div class="value-card group svelte-avlyqk"><div class="card-content svelte-avlyqk"><div class="icon-wrapper group-hover svelte-avlyqk"><!></div> <h4 class="card-title svelte-avlyqk">Opportunity</h4> <p class="card-text svelte-avlyqk">Creating pathways for local talent to shine and providing mentorship for the next generation of players.</p></div></div></div></div></section>');function de(i){var e=ce(),t=n(s(e),2),r=n(s(t),2),o=s(r),m=s(o),l=s(m),v=s(l);ae(v,{class:"icon"}),a(l),b(4),a(m),a(o);var y=n(o,2),f=s(y),g=s(f),w=s(g);te(w,{class:"icon"}),a(g),b(4),a(f),a(y);var x=n(y,2),q=s(x),M=s(q),j=s(M);se(j,{class:"icon"}),a(M),b(4),a(q),a(x),a(r),a(t),a(e),d(i,e)}var me=u('<span class="text-block svelte-1i9z5gq">GAME DAY</span> <span class="text-block svelte-1i9z5gq">GAME DAY</span> <span class="text-block svelte-1i9z5gq">GAME DAY</span>',1),pe=u('<span class="address svelte-1i9z5gq"> </span>'),ue=u('<div class="video-wrapper svelte-1i9z5gq"><iframe width="560" height="315" title="Somerville United FC Game Livestream" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe></div>'),ge=u('<div class="video-link-wrapper svelte-1i9z5gq"><a target="_blank" rel="noopener noreferrer" name="livestream"><!></a></div>'),he=u('<span class="text-block svelte-1i9z5gq">GAME DAY</span> <span class="text-block svelte-1i9z5gq">GAME DAY</span> <span class="text-block svelte-1i9z5gq">GAME DAY</span>',1),ye=u('<div class="wrapper svelte-1i9z5gq"><div class="container svelte-1i9z5gq"><section><p class="marquee svelte-1i9z5gq"></p> <h3 class="svelte-1i9z5gq"> <!></h3> <!> <!> <p class="marquee svelte-1i9z5gq"></p></section></div></div>');function fe(i,e){O(e,!0);let t=A(()=>e.game.Home==="Somerville United FC");const r=A(()=>V(e.game)),o=A(()=>B(e.game));var m=ye(),l=s(m),v=s(l),y=s(v);N(y,20,()=>Array(10),U,(c,p)=>{var h=me();b(4),d(c,h)}),a(y);var f=n(y,2),g=s(f),w=n(g);{var x=c=>{var p=pe(),h=s(p,!0);a(p),k(()=>G(h,e.game.Address)),d(c,p)};$(w,c=>{e.game.Address&&c(x)})}a(f);var q=n(f,2);{var M=c=>{var p=ue(),h=s(p);a(p),k(()=>S(h,"src",`https://www.youtube.com/embed/${e.game.YouTubeId??""}`)),d(c,p)};$(q,c=>{_(r)&&c(M)})}var j=n(q,2);{var F=c=>{var p=ge(),h=s(p),I=s(h);Z(I,{size:"100",color:"#ffffff"}),a(h),a(p),k(()=>S(h,"href",e.game.YouTubeId)),d(c,p)};$(j,c=>{_(o)&&c(F)})}var D=n(j,2);N(D,20,()=>Array(10),U,(c,p)=>{var h=he();b(4),d(c,h)}),a(D),a(v),a(l),a(m),k(()=>G(g,`${_(t)?"vs":"at"}
+        ${(_(t)?e.game.Away:e.game.Home)??""}
+        @ ${e.game.Time??""} `)),d(i,m),E()}var be=u(`<section id="community" class="community-section svelte-byxqdl"><div class="container"><div class="community-grid svelte-byxqdl"><div class="community-content"><h2 class="section-eyebrow">Latest News</h2> <h3>Now playing in Somerville</h3> <div class="community-text"><p class="text-md svelte-byxqdl">Our men's teams are now playing in various local leagues leagues. <a href="/schedule">Check out our schedule</a> to join us at one of our upcoming games.</p></div></div> <div class="community-image"><a href="https://www.missingmoney.com" target="_blank" rel="noopener noreferrer"><img alt="Missing Money Logo" class="logo-large svelte-byxqdl"/></a> <p class="text-sm svelte-byxqdl"><a href="https://www.missingmoney.com" target="_blank" rel="noopener noreferrer">MissingMoney.com</a> is our jersey sponsor for the 2026 season. They are a free online database 
+            that helps people find and claim unclaimed property, such as forgotten bank accounts, uncashed 
+            checks, and other assets. We are proud to partner with <a href="https://www.missingmoney.com" target="_blank" rel="noopener noreferrer">MissingMoney.com</a> to help raise awareness 
+            about their valuable service while also supporting our team on the field.</p></div></div> <!></div></section>`);function _e(i){var e=be(),t=s(e),r=s(t),o=n(s(r),2),m=s(o),l=s(m);a(m),b(2),a(o),a(r);var v=n(r,2);R(v,{}),a(t),a(e),k(()=>S(l,"src",ee)),d(i,e)}var we=u('<meta name="description" content="Somerville United FC - Building community through soccer"/>'),qe=u("<!> <!> <!> <!> <!>",1);function Ne(i,e){O(e,!0);const t=A(()=>J(e.data.calendars));var r=qe();Q("1uha8ag",g=>{var w=we();W(()=>{K.title="Somerville United FC - Home"}),d(g,w)});var o=z(r);{var m=g=>{fe(g,{get game(){return _(t)[0]}})};$(o,g=>{_(t).length>0&&g(m)})}var l=n(o,2);le(l);var v=n(l,2);ve(v);var y=n(v,2);de(y);var f=n(y,2);_e(f),d(i,r),E()}export{Ne as component,Ge as universal};
