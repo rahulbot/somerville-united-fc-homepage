28bad7cb2770a16d473c4c842a6308e75c024e5c
GitHub Action <action@github.com>
2026-09-18 19:41:01 +0000

    GH-Bot: Rebuild Svelte app for release

---
+++ docs/_app/immutable/nodes/5.CHt5HRfB.js
@@ -0,0 +1,88 @@
+import{l as We,c as Ce,P as Ie,d as Ve,e as Ke}from"../chunks/B8CU2a6g.js";import{c as le,a as l,f as o,k as Xe,e as Ye,j as I,t as Je}from"../chunks/CmPzn-1z.js";import{o as K,aF as he,af as s,E as e,ag as r,ad as n,ab as N,aG as be,aj as b,ai as p,ak as V,ac as Qe,ae as Ze,bj as $e,aH as ea}from"../chunks/BvqWAM62.js";import{s as q}from"../chunks/nTMJ3izd.js";import{i as j}from"../chunks/DKUflK--.js";import{e as pe,i as _e}from"../chunks/DhcGsI9D.js";import{h as aa}from"../chunks/DWgOyVH-.js";import{s as Le,a as D,c as ta}from"../chunks/Ciyof1AA.js";import{G as ra,a as sa,L as la}from"../chunks/M3rzp3z3.js";import{S as na}from"../chunks/BT0PY9y4.js";import"../chunks/CSFTXAge.js";import{I as qe,s as Ne}from"../chunks/rVhMkKb6.js";import{l as xe,s as Pe}from"../chunks/BrbSwlt6.js";import{X as oa}from"../chunks/Blnp6unu.js";function ia(x,a){const y=xe(a,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.555.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const m=[["path",{d:"M8 2v4"}],["path",{d:"M16 2v4"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2"}],["path",{d:"M3 10h18"}]];qe(x,Pe({name:"calendar"},()=>y,{get iconNode(){return m},children:(_,M)=>{var g=le(),v=K(g);Ne(v,a,"default",{}),l(_,g)},$$slots:{default:!0}}))}function ca(x,a){const y=xe(a,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.555.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const m=[["rect",{width:"14",height:"14",x:"8",y:"8",rx:"2",ry:"2"}],["path",{d:"M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"}]];qe(x,Pe({name:"copy"},()=>y,{get iconNode(){return m},children:(_,M)=>{var g=le(),v=K(g);Ne(v,a,"default",{}),l(_,g)},$$slots:{default:!0}}))}const va=!1,da=({fetch:x,params:a})=>({calendar:We(x)}),Ja=Object.freeze(Object.defineProperty({__proto__:null,load:da,ssr:va},Symbol.toStringTag,{value:"Module"}));var ua=o('<span class="chip postponed svelte-4d7j02">Postponed</span>'),ma=o('<span class="note svelte-4d7j02"> </span>'),ga=o('<span class="chip win svelte-4d7j02">Win <!></span>'),fa=o('<span class="note svelte-4d7j02"> </span>'),pa=o('<span class="chip loss svelte-4d7j02">Loss <!></span>'),_a=o('<span class="note svelte-4d7j02"> </span>'),ha=o('<span class="chip draw svelte-4d7j02">Draw <!></span>'),ba=o('<a target="_blank"><button class="btn-primary svelte-4d7j02">Get Tickets</button></a>'),ya=o('<a><button class="btn-primary svelte-4d7j02">RSVP</button></a>'),Sa=o('<div><div class="svelte-4d7j02"><!></div> <div class="svelte-4d7j02"><!></div> <div class="status-wrapper svelte-4d7j02"><!></div></div>');function ka(x,a){he(a,!0);const y=b(()=>a.game.Home===a.teamName),m=b(()=>a.game.Postponed==="Yes"),_=b(()=>a.game.Round!="Regular Season"),M=b(()=>!!a.game.parsedDate&&new Date>new Date(a.game.parsedDate)),g=b(()=>a.includeTicketButton&&e(y)&&a.game.Tickets&&a.game.Tickets.startsWith("http"));var v=Sa();let X;var d=s(v),ne=s(d);ra(ne,{get game(){return a.game},get isPostSeason(){return e(_)}}),r(d);var z=n(d,2),$=s(z);sa($,{get game(){return a.game},get isHome(){return e(y)},get isPostSeason(){return e(_)}}),r(z);var B=n(z,2),J=s(B);{var ee=h=>{var C=ua();l(h,C)},G=h=>{var C=le(),Q=K(C);{var te=P=>{var f=ga(),U=n(s(f));{var O=S=>{var k=ma(),E=s(k,!0);r(k),N(()=>q(E,a.game.Note)),l(S,k)};j(U,S=>{a.game.Note&&S(O)})}r(f),l(P,f)},ie=P=>{var f=pa(),U=n(s(f));{var O=S=>{var k=fa(),E=s(k,!0);r(k),N(()=>q(E,a.game.Note)),l(S,k)};j(U,S=>{a.game.Note&&S(O)})}r(f),l(P,f)},re=P=>{var f=ha(),U=n(s(f));{var O=S=>{var k=_a(),E=s(k,!0);r(k),N(()=>q(E,a.game.Note)),l(S,k)};j(U,S=>{a.game.Note&&S(O)})}r(f),l(P,f)};j(Q,P=>{a.game.Result=="W"?P(te):a.game.Result=="L"?P(ie,1):a.game.Result=="D"&&P(re,2)})}l(h,C)},oe=h=>{var C=ba();N(()=>D(C,"href",a.game.Tickets)),l(h,C)},ae=h=>{var C=ya();N(()=>D(C,"href",`tickets?game=${a.game.id??""}`)),l(h,C)};j(J,h=>{e(m)?h(ee):e(M)||a.game.Result?h(G,1):e(g)?h(oe,2):a.game.RSVPable&&h(ae,3)})}r(B),r(v),N(()=>X=Le(v,1,"game-row svelte-4d7j02",null,X,{past:e(M)||a.game.Result})),l(x,v),be()}var wa=o("<p>🚧 No games scheduled yet. Check back soon!</p>"),Ca=o("<!> <!>",1);function La(x,a){he(a,!0);var y=Ca(),m=K(y);{var _=g=>{var v=wa();l(g,v)};j(m,g=>{a.games.length===0&&g(_)})}var M=n(m,2);pe(M,17,()=>a.games,_e,(g,v)=>{ka(g,{get game(){return e(v)},get teamName(){return a.teamName},get includeTicketButton(){return a.includeTicketButton}})}),l(x,y),be()}var qa=o('<meta name="description" content="Keep track of fixtures and results"/>'),Na=o("<span>Loading calendar...</span>"),xa=o('<em class="svelte-19rgvlq"> </em> Days Till Our Home Opener',1),Pa=o("<option> </option>"),Ta=o('<button role="tab"><div class="league-logo svelte-19rgvlq"><!></div> </button>'),ja=o('<p class="copy-notice svelte-19rgvlq">copied to clipboard</p>'),Da=o('<div class="modal-backdrop svelte-19rgvlq" role="presentation"><div class="modal svelte-19rgvlq" role="dialog" aria-modal="true" aria-labelledby="calendar-modal-title" tabindex="-1"><button class="modal-close-icon svelte-19rgvlq" type="button" aria-label="Close calendar subscription modal"><!></button> <h4 id="calendar-modal-title" class="svelte-19rgvlq"> </h4> <p class="svelte-19rgvlq">Subscribe to stay up-to-date with our latest games. Use this feed URL in your favorite calendar app:</p> <div class="copy-field svelte-19rgvlq" role="group" aria-label="Calendar feed URL"><span class="copy-field-url svelte-19rgvlq"> </span> <button class="copy-field-btn svelte-19rgvlq" type="button" aria-label="Copy calendar feed URL to clipboard"><!></button></div> <!> <ul class="svelte-19rgvlq"><li><strong>iPhone (Apple Calendar):</strong> Open Calendar, tap the calendar icon at the bottom right, scroll down and tap "Add Calendar". Pick "Add Subscription Calendar" and paste in the URL.</li></ul> <div class="modal-actions svelte-19rgvlq"><a class="btn-feed-link svelte-19rgvlq" target="_blank" rel="noopener noreferrer">or download current .ics</a></div></div></div>'),Ua=o('<div class="season-picker svelte-19rgvlq"><label for="season-select" class="svelte-19rgvlq">Season</label> <select id="season-select" class="svelte-19rgvlq"></select></div> <div class="tabs svelte-19rgvlq" role="tablist" aria-label="Select Team"></div> <div role="tabpanel" class="tab-panel svelte-19rgvlq"><div class="panel-heading svelte-19rgvlq"><h3 class="svelte-19rgvlq"> </h3> <button class="btn-subscribe svelte-19rgvlq" type="button"><!> </button></div> <p class="svelte-19rgvlq"> </p> <!></div> <!>',1),Oa=o('<div class="schedule-loading svelte-19rgvlq"><!></div>'),Aa=o(`<div class="container"><section><h1><!></h1> <p class="page-subtitle">Somerville United FC fields men's teams in various local leagues. We are currently playing in the <a href="https://division1.upsl.com/teams/somerville-united-fc-936815/">UPSL Division 1 New England conference</a> and <a href="https://apslsoccer.com/APSL/Team/165459">APSL Mayflower Conference</a>.
+      We'd love to have you join at one of our upcoming games!</p> <!></section> <section><!></section></div>`);function Qa(x,a){he(a,!0);let y=V(!0),m=V(null),_=V(null);a.data.calendar.then(t=>{p(m,t,!0),p(_,Ce(t),!0),p(y,!1)});const M=b(()=>e(m)?Object.keys(e(m)):[]),g=b(()=>e(m)?Ce(e(m)):null);let v=b(()=>e(m)&&e(_)?e(m)[e(_)]:null),X=b(()=>e(v)?Object.keys(e(v)):[]),d=b(()=>e(X)[0]);function ne(t){switch(t){case"UPSL":return"Our men's team plays in the UPSL Division 1 New England Conference. Home games are at Dilboy Stadum in Somerville.";case"APSL":return"Our men's team plays in the APSL Mayflower Conference. Home games are at Dilboy Stadum in Somerville.";case"CASA":return"Our men's team plays in the CASA Select Liga 1. Home games are at Conway Park in Somerville.";case"D-One":return"Our men's team plays in the D-One Soccer League.";case"US Open Cup":return"The US Open Cup is a knockout cup competition across all levels of American soccer. We play it in when we win a competition in one of our other leagues.";default:return""}}const z=b(()=>e(v)?e(v)[0].parsedDate:null);let $=b(()=>{let t;try{const i=new Date;if(e(z)&&i<e(z)){const w=(new Date(e(z))-i)/864e5;t=Math.ceil(w)}}catch{t=void 0}return t}),B=V(!1),J=V(""),ee=V(""),G=V(!1);const oe={"Somerville United FC":"Men's Flagship","Somerville United FC II":"Men's Reserve"},ae=b(()=>{const t={};return e(X).forEach(i=>{const w=e(v)[i],H=w[0].Home.includes("Somerville United")?w[0].Home:w[0].Away,W=oe[H]||H;t[i]={label:W,url:`/calendars/${Ke(i)}`}}),t});function h(t){(t.key==="ArrowLeft"||t.key==="ArrowRight")&&(t.preventDefault(),p(d,e(d)==="apsl"?"casa":"apsl"),setTimeout(()=>{document.getElementById(`${e(d)}-tab`)?.focus()},0))}function C(t){p(J,e(ae)[t].url,!0),p(ee,e(ae)[t].label,!0),p(G,!1),p(B,!0)}function Q(){p(B,!1),p(G,!1)}function te(t){e(B)&&t.key==="Escape"&&Q()}function ie(t){t.target===t.currentTarget&&Q()}const re=b(()=>`http://somervilleunitedfc.org${e(J)}`);async function P(){try{await navigator.clipboard.writeText(e(re)),p(G,!0),setTimeout(()=>{p(G,!1)},4e3)}catch{p(G,!1)}}var f=Aa();aa("19rgvlq",t=>{var i=qa();Qe(()=>{Ze.title="Schedule - Somerville United FC"}),l(t,i)}),Ye("keydown",$e,te);var U=s(f),O=s(U),S=s(O);{var k=t=>{var i=Na();l(t,i)},E=t=>{var i=le(),w=K(i);{var H=L=>{var A=xa(),Y=K(A),ce=s(Y,!0);r(Y),ea(),N(()=>q(ce,e($))),l(L,A)},W=L=>{var A=Je();N(()=>q(A,`${e(g)??""} Season Schedule`)),l(L,A)};j(w,L=>{e($)?L(H):L(W,-1)})}l(t,i)};j(S,t=>{e(y)?t(k):t(E,-1)})}r(O);var Te=n(O,4);{var je=t=>{var i=Ua(),w=K(i),H=n(s(w),2);pe(H,21,()=>e(M),_e,(T,c)=>{var u=Pa(),R=s(u,!0);r(u);var F={};N(()=>{q(R,e(c)),F!==(F=e(c))&&(u.value=(u.__value=e(c))??"")}),l(T,u)}),r(H),r(w);var W=n(w,2);pe(W,21,()=>e(X),_e,(T,c)=>{var u=Ta();let R;var F=s(u),Z=s(F);la(Z,{get league(){return e(c)}}),r(F);var de=n(F);r(u),N(()=>{R=Le(u,1,"tab svelte-19rgvlq",null,R,{active:e(d)===e(c),inactive:e(d)!==e(c)}),D(u,"aria-selected",e(d)===e(c)),D(u,"aria-controls",`${e(c)}-panel`),D(u,"id",`${e(c)}-tab`),D(u,"tabindex",e(d)===e(c)?0:-1),q(de,` ${e(c)??""}`)}),I("click",u,()=>p(d,e(c))),I("keydown",u,h),l(T,u)}),r(W);var L=n(W,2),A=s(L),Y=s(A),ce=s(Y,!0);r(Y);var se=n(Y,2),Se=s(se);ia(Se,{size:16,"aria-hidden":"true"});var Oe=n(Se);r(se),r(A);var ve=n(A,2),Ae=s(ve,!0);r(ve);var Re=n(ve,2);La(Re,{get games(){return e(v)[e(d)]},teamName:"Somerville United FC",includeTicketButton:!0}),r(L);var Me=n(L,2);{var Ge=T=>{var c=Da(),u=s(c),R=s(u),F=s(R);oa(F,{size:18,"aria-hidden":"true"}),r(R);var Z=n(R,2),de=s(Z);r(Z);var ue=n(Z,4),me=s(ue),He=s(me,!0);r(me);var ge=n(me,2),Fe=s(ge);ca(Fe,{size:16,"aria-hidden":"true"}),r(ge),r(ue);var ke=n(ue,2);{var ze=fe=>{var Ee=ja();l(fe,Ee)};j(ke,fe=>{e(G)&&fe(ze)})}var we=n(ke,4),Be=s(we);r(we),r(u),r(c),N(()=>{q(de,`Subscribe to ${e(ee)??""} Calendar`),q(He,e(re)),D(Be,"href",e(J))}),I("click",c,ie),I("keydown",c,te),I("click",R,Q),I("click",ge,P),l(T,c)};j(Me,T=>{e(B)&&T(Ge)})}N((T,c)=>{D(L,"id",`${e(d)}-panel`),D(L,"aria-labelledby",`${e(d)}-tab`),q(ce,T),D(se,"aria-label",`Subscribe to our ${e(d)} league calendar`),q(Oe,` add ${e(d)??""} games to your calendar`),q(Ae,c)},[()=>Ve(e(v)[e(d)]),()=>ne(e(d))]),ta(H,()=>e(_),T=>p(_,T)),I("click",se,()=>C(e(d))),l(t,i)},De=t=>{var i=Oa(),w=s(i);na(w,{size:28,label:"Loading schedule"}),r(i),l(t,i)};j(Te,t=>{e(y)?t(De,-1):t(je)})}r(U);var ye=n(U,2),Ue=s(ye);Ie(Ue,{}),r(ye),r(f),l(x,f),be()}Xe(["click","keydown"]);export{Qa as component,Ja as universal};
